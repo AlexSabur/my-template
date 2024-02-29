0f2e4af614a7ea10868a366177985a4b65042776
Сабуров Александр Владимирович <a.saburov@softmg.ru>
2024-02-29 23:29:44 +0300

    update

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -5,14 +5,15 @@ module.exports = {
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
-    'plugin:@conarti/feature-sliced/recommended',
+    '@feature-sliced',
+    'plugin:@conarti/eslint-plugin-feature-sliced/recommended',
     'plugin:effector/future',
     'plugin:effector/patronum',
     'plugin:effector/react',
     'plugin:effector/recommended',
     'plugin:effector/scope',
   ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  ignorePatterns: ['dist', '.eslintrc.cjs', 'tailwind.config.ts'],
   parser: '@typescript-eslint/parser',
   settings: {
     "import/resolver": {
@@ -27,14 +28,35 @@ module.exports = {
       'warn',
       { allowConstantExport: true },
     ],
+    'import/no-internal-modules': [
+      'error', 
+      {
+        allow: [
+          'react-dom/client',
+          // 
+          '**/shared/i18n',
+          '**/shared/router',
+          '**/shared/config',
+        ]
+      }
+    ],
+    '@conarti/feature-sliced/absolute-relative': [
+      'error',
+      { 
+        ignoreInFilesPatterns: [
+          '**/shared/router/*.{ts,tsx}',
+        ],
+      },
+    ],
     '@conarti/feature-sliced/public-api': [
       'error',
       { 
         ignoreInFilesPatterns: [
+          '**/src/client-entry.{ts,tsx}',
           '**/src/app/index.{ts,tsx}',
           '**/src/pages/index.{ts,tsx}',
         ],
       },
-    ]
+    ],
   },
 }
